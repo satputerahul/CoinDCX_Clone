bc614729db6de74d340dd5c61914fd7778541a23
Rahul Satpute <126018769+satputerahul@users.noreply.github.com>
2024-01-01 12:50:30 +0530

    Update myCollectionViewCell.swift

--- CoinDCX_Clone/myCollectionViewCell.swift
+++ CoinDCX_Clone/myCollectionViewCell.swift
@@ -1,14 +1,7 @@
-//
-//  myCollectionViewCell.swift
-//  CoinDCX_Clone
-//
-//  Created by DevMAC01 on 15/12/23.
-//
 
 import UIKit
 
 class myCollectionViewCell: UICollectionViewCell {
-    
     
     @IBOutlet var navImg: UIImageView!
     @IBOutlet var navLbl: UILabel!
